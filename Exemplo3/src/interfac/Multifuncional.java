474a9a114fcfe54bcb9d6da4b63d8c539144a5ee
marcelobentes <marcelo.maia.m@gmail.com>
2023-02-16 16:33:05 -0300

    alteração de teste

--- Exemplo3/src/interfac/Multifuncional.java
+++ Exemplo3/src/interfac/Multifuncional.java
@@ -4,18 +4,18 @@ public class Multifuncional implements Digitalizadora, Copiadora, Impressora{
 
 	@Override
 	public void scannear() {
-		System.out.println("DIGITALIZANDO MULTIFUNCIONAL.");
+		System.out.println("DIGITALIZANDO MULTIFUNCIONAL...");
 	}
 
 	@Override
 	public void imprimir() {
-		System.out.println("IMPRIMINDO MULTIFUNCIONAL.");
+		System.out.println("IMPRIMINDO MULTIFUNCIONAL...");
 		
 	}
 
 	@Override
 	public void copiar() {
-		System.out.println("COPIANDO MULTIFUNCIONAL.");
+		System.out.println("COPIANDO MULTIFUNCIONAL...");
 		
 	}
 
